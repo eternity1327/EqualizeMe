478fa1c62c4938fac7963b42836cc073c36b0d6c
Eternity <gianopsima@gmail.com>
2026-08-02 19:57:11 +0800

    none

--- utils/mysqlPool.js
+++ utils/mysqlPool.js
@@ -1,12 +1,3 @@
-/**
- * utils/mysqlPool.js
- *
- * Central MySQL connection pool. All routes that need the database
- * import { pool } from here and use pool.execute(...) / pool.query(...).
- *
- * Credentials come from .env (never hardcode them in this file).
- */
-
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
